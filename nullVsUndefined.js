d281c8091bbc8d11d6b3343df50baf9be324be01
UtshoBarua <utsho45-211@diu.edu.bd>
2021-01-02 00:53:09 +0600

    null vs undefined

---
+++ nullVsUndefined.js
@@ -0,0 +1,12 @@
+let pakhi
+{
+    console.log(pakhi)
+}
+
+function add (num1,num2){
+    console.log(num1 + num2)
+}
+
+const result = add(50, 20)
+console.log(result)
+
---
+++ trueFalse.js
@@ -0,0 +1,7 @@
+const age = 5;
+if(age>0){
+    console.log("its fine")
+}
+else{
+    console.log("its not fine")
+}
